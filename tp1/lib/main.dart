824d20db63da9140e4607787e7666fd5bbbe6f8b
mateo_ordi_principal <mateo.rinaldi54@gmail.com>
2024-02-12 11:13:18 +0100

    adding a like button (not usable)

--- tp1/lib/main.dart
+++ tp1/lib/main.dart
@@ -23,20 +23,6 @@ class BottomNavigationBarMenu extends StatefulWidget {
 class _BottomNavigationBarMenuState extends State<BottomNavigationBarMenu> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Series',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Musiques',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: XX',
-      style: optionStyle,
-    ),
-  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -46,15 +32,29 @@ class _BottomNavigationBarMenuState extends State<BottomNavigationBarMenu> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _widgetOptions = <Widget>[
+      _buildListView(), // This should be inside the build method
+      const Text(
+        'Index 1: Musiques',
+        style: optionStyle,
+      ),
+      const Text(
+        'Index 2: XX',
+        style: optionStyle,
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: const Center(
-          child: Text('AMSE - Application Musique Serie Ensemble')
+          child: Text(
+            'AMSE - Application de Musiques et Series Ensemble',
+            style: optionStyle,
           ),
+        ),
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -76,4 +76,59 @@ class _BottomNavigationBarMenuState extends State<BottomNavigationBarMenu> {
       ),
     );
   }
+
+  static const List<String> description = <String>[
+    'A', 
+    'B', 
+    'C'
+    ];
+  static const List<String> images = <String>['test.jpg', 'test.jpg', 'test.jpg'];
+
+  Widget _buildListView() {
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: description.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18), // Vous pouvez ajuster le rayon selon votre préférence
+            color: Colors.black26,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: [
+                IconButton(
+                onPressed: () {
+                  // appState.toggleFavorite();
+                },
+                icon: const Icon(Icons.favorite),
+              ),
+
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image(
+                    image: AssetImage('assets/images/${images[index]}'),
+                    height: 100,
+                    alignment: Alignment.centerLeft,
+                    ),
+                ),
+
+                const SizedBox(width: 8,),
+
+                Flexible(
+                  child: Text(
+                  'Description\njifgje ${description[index]}',
+                  ),
+                )
+              ]
+              ),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  
 }
